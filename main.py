862fa404f672ae06508592ed0723b34c72617379
nalin-mathur15 <mathurnalin15@googlemail.com>
2025-02-03 18:09:02 +0800

    set up game

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pathfinding Algorithm') #replace with some options thingy
 
+def grid():
+    for row in range(ROWS):
+        for col in range(ROWS):
+            pygame.draw.rect(screen, WHITE, (col * CELL, row * CELL, CELL, CELL))
+            pygame.draw.rect(screen, GRAY, (col * CELL, row * CELL, CELL, CELL), 1)
+
+def main():
+    game = True
+    while game:
+        screen.fill(WHITE)
+        grid()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+        
+        pygame.display.update()
+    
+    pygame.quit()
+    sys.quit()
+
+if __name__ == '__main__':
+    main()
 
 
 
